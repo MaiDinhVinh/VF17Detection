630ea7925a8307b986eaa99141cb6e43cc0c3bf4
DucksaberVN <business@ducksabervn.com>
2025-10-16 10:04:17 +0700

    Update VF17Main.java

--- src/main/java/com/arthroverse/vf17/main/VF17Main.java
+++ src/main/java/com/arthroverse/vf17/main/VF17Main.java
@@ -1,20 +1,38 @@
 package com.arthroverse.vf17.main;
 
-import com.arthroverse.vf17.detection.InitDetection;
+import com.arthroverse.vf17.uicontrollers.HomepageUIController;
+import io.github.palexdev.materialfx.theming.JavaFXThemes;
+import io.github.palexdev.materialfx.theming.MaterialFXStylesheets;
+import io.github.palexdev.materialfx.theming.UserAgentBuilder;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class VF17Main {
+public class VF17Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        primaryStage.setTitle("nigger");
+        UserAgentBuilder.builder()
+                .themes(JavaFXThemes.MODENA) // Optional if you don't need JavaFX's default theme, still recommended though
+                .themes(MaterialFXStylesheets.forAssemble(true)) // Adds the MaterialFX's default theme. The boolean argument is to include legacy controls
+                .setDeploy(true) // Whether to deploy each theme's assets on a temporary dir on the disk
+                .setResolveAssets(true) // Whether to try resolving @import statements and resources urls
+                .build() // Assembles all the added themes into a single CSSFragment (very powerful class check its documentation)
+                .setGlobal(); // Finally, sets the produced stylesheet as the global User-Agent stylesheet//Once everything has been set up, redirect the user to the authenication form
+
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/fxml/HomepageUI.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        HomepageUIController controller = loader.getController();
+        controller.handleShutdown(primaryStage);
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
-        try {
-            InitDetection init = new InitDetection();
-
-            // For webcam
-            init.startWebcam();
-
-            // For video file
-            // init.startVideo("path/to/your_video.mp4");
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        launch(args);
     }
 }
