37bad3b25bd0490ef0e838a5a94c3070872752d0
DucksaberVN <business@ducksabervn.com>
2025-10-25 01:20:40 +0700

    proper comments added

--- src/main/java/com/arthroverse/vf17/uicontrollers/HomepageUIController.java
+++ src/main/java/com/arthroverse/vf17/uicontrollers/HomepageUIController.java
@@ -1,3 +1,22 @@
+/******************************************************************************
+ * Project Name:    [Gumball - VF17Detection]
+ * Course:          [CECS1011 - Intro to CECS]
+ * Semester:        [Fall 2025]
+ * <p>
+ * Members:         Dinh Hieu Minh <25minh.dh2@vinuni.edu.vn>,
+ *                  Duc Phat Hoang <25phat.hd@vinuni.edu.vn>,
+ *                  Le Ngoc Han <25han.ln@vinuni.edu.vn>,
+ *                  Ngo Van Thang <25thang.nv@vinuni.edu.vn>,
+ *                  Mai Dinh Vinh <25vinh.md@vinuni.edu.vn>
+ * <p>
+ * Date Created:    [10-15-2025]
+ * Last Modified:   [10-22-2025]
+ * <p>
+ * File Name:       [HomepageUIController.java]
+ * Developer:       Le Ngoc Han
+ * Description:     [JavaFX UI controller, controlling what happen on the frontend]
+ ******************************************************************************/
+
 package com.arthroverse.vf17.uicontrollers;
 
 import com.arthroverse.vf17.detection.DetectionHandler;
@@ -7,7 +26,6 @@ import javafx.animation.AnimationTimer;
 import javafx.application.Platform;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
@@ -41,6 +59,15 @@ public class HomepageUIController implements Initializable {
 
     private static int MAX_RECORD = 10;
 
+    /**
+     * This method will set everything up like the ImageView integration with
+     * Java AWT Buffered Image so that it acts like a video camera view, initalize the
+     * detection handler, or anything else you need to setup before initialize the UI
+     *
+     * @author Le Ngoc Han
+     *
+     * @Version 1.0
+     */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         try {
@@ -50,7 +77,6 @@ public class HomepageUIController implements Initializable {
             detectionHandler = Handler;
             setupFrameUpdater();
 
-            // Configure ImageView for smooth display
             mainCamView.setPreserveRatio(true);
             mainCamView.setSmooth(true);
 
@@ -60,7 +86,6 @@ public class HomepageUIController implements Initializable {
             BufferedImage bufferedImage = detectionHandler.getLatestFrame();
 
             if (bufferedImage != null) {
-                // Convert BufferedImage to JavaFX Image
                 Image fxImage = SwingFXUtils.toFXImage(bufferedImage, null);
                 mainCamView.setImage(fxImage);
             }
@@ -76,6 +101,13 @@ public class HomepageUIController implements Initializable {
         }
     }
 
+    /**
+     * This method sets an animation timer which will assign a new frame to the ImageView
+     * and acts like video camera frame
+     * @author Le Ngoc Han
+     *
+     * @Version 1.0
+     */
     private void setupFrameUpdater() {
         frameUpdater = new AnimationTimer() {
             @Override
@@ -85,16 +117,29 @@ public class HomepageUIController implements Initializable {
         };
     }
 
+    /**
+     * This method will get the latest frame from OpenCV camera implementation and
+     * assign it to the ImageView
+     * @author Le Ngoc Han
+     *
+     * @Version 1.0
+     */
     private void updateFrame() {
         BufferedImage bufferedImage = detectionHandler.getLatestFrame();
 
         if (bufferedImage != null) {
-            // Convert BufferedImage to JavaFX Image
             Image fxImage = SwingFXUtils.toFXImage(bufferedImage, null);
             mainCamView.setImage(fxImage);
         }
     }
 
+    /**
+    * This method will cleanup all resources such as camera and YOLO Inference objects
+    * such as Concurrency objects,...
+    * @author Le Ngoc Han
+    *
+    * @Version 1.0
+    * */
     public void shutdown() {
         if (frameUpdater != null) {
             frameUpdater.stop();
@@ -104,12 +149,28 @@ public class HomepageUIController implements Initializable {
         }
     }
 
+    /**
+     * This method will assign a listener to the current UI and will initiate
+     * the shutdown whenever the user close the windows
+     * @author Le Ngoc Han
+     *
+     * @Version 1.0
+     */
     public void handleShutdown(Stage stage){
         stage.setOnCloseRequest(event -> {
             shutdown();
         });
     }
 
+    /**
+     * This method will add inference output to a separate VBox for more information to the user
+     * This method not only add the inference output but also add some CSS effects to the text
+     * like fonts, size, color based on the output
+     *
+     * @author Le Ngoc Han
+     *
+     * @Version 1.0
+     */
     public static void frontendUpdateOutput(String inferOutput, boolean isRotten){
         Platform.runLater(() -> {
             Label label = new Label(inferOutput);
