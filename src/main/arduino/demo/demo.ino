6eaf73bc2b15c1255a507bb540d0fe0f7fe9eafd
DucksaberVN <business@ducksabervn.com>
2026-01-20 18:11:14 +0700

    Update demo.ino

--- src/main/arduino/demo/demo.ino
+++ src/main/arduino/demo/demo.ino
@@ -1,12 +1,28 @@
+#include <Servo.h>
+
+#define SERVO_PIN 9
+
+Servo myservo;
+
+const int HOME_ANGLE = 0;
+const int PUSH_ANGLE = 180;
+
 void setup() {
   Serial.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);
+
+  myservo.attach(SERVO_PIN); //tin hieu
+  myservo.write(HOME_ANGLE);
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    char c = Serial.read();
-    if (c == '1') digitalWrite(LED_BUILTIN, HIGH);
-    if (c == '0') digitalWrite(LED_BUILTIN, LOW);
+
+  if (Serial.available()) {
+    char cmd = Serial.read();
+
+    if (cmd == '1') {
+      myservo.write(HOME_ANGLE);
+      delay(1000);             
+      myservo.write(PUSH_ANGLE);
+    }
   }
 }
