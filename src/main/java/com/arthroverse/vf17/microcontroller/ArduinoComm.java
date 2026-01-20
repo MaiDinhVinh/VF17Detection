1aa21dbfd20a9fd786577bbff6d0c97bc8998789
DucksaberVN <business@ducksabervn.com>
2026-01-20 18:12:02 +0700

    Update ArduinoComm.java

--- src/main/java/com/arthroverse/vf17/microcontroller/ArduinoComm.java
+++ src/main/java/com/arthroverse/vf17/microcontroller/ArduinoComm.java
@@ -1,3 +1,22 @@
+/******************************************************************************
+ * Project Name:    [Gumball - VF17Detection]
+ * Course:          [CECS1011 - Intro to CECS]
+ * Semester:        [Fall 2025]
+ * <p>
+ * Members:         Dinh Hieu Minh <25minh.dh2@vinuni.edu.vn>,
+ *                  Duc Phat Hoang <25phat.hd@vinuni.edu.vn>,
+ *                  Le Ngoc Han <25han.ln@vinuni.edu.vn>,
+ *                  Ngo Van Thang <25thang.nv@vinuni.edu.vn>,
+ *                  Mai Dinh Vinh <25vinh.md@vinuni.edu.vn>
+ * <p>
+ * Date Created:    [10-15-2025]
+ * Last Modified:   [10-22-2025]
+ * <p>
+ * File Name:       [ArduinoComm.java]
+ * Developer:       Dinh Hieu Minh
+ * Description:     [Java communication with Arduino Controller using jSerialComm]
+ ******************************************************************************/
+
 package com.arthroverse.vf17.microcontroller;
 
 import com.fazecast.jSerialComm.SerialPort;
@@ -9,44 +28,52 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * Provides a minimal utility to send serial commands from Java to an Arduino controller
+ * using the jSerialComm library.
+ *
+ * @author Dinh Hieu Minh (25minh.dh2@vinuni.edu.vn)
+ * @version 1.0
+ */
 public class ArduinoComm {
-    private static final int  BAUD_RATE = 9600;
+
+    private static final int BAUD_RATE = 9600;
     private static final int NUM_DATA_BITS = 8;
     private static final int NUM_DATA_STOP_BITS = 1;
-    private static final SerialPort PORT= SerialPort.getCommPort("/dev/tty.usbmodem21201");
+    private static final SerialPort PORT = SerialPort.getCommPort("/dev/tty.usbmodem11101");
 
-    public static final void COMMUNICATE(boolean isRotten) throws IOException, InterruptedException {
+    /**
+     * Sends a single-byte command to Arduino via serial port to indicate whether the current
+     * detected object is rotten.
+     * <p>
+     * This method writes {@code "1"} if {@code isRotten} is {@code true}, otherwise writes {@code "0"}.
+     *
+     * @param isRotten {@code true} to send rotten signal; {@code false} to send fresh signal
+     * @throws IOException if an I/O error occurs when writing to the serial port
+     * @throws InterruptedException declared for compatibility with callers that may coordinate timing;
+     *                              this method does not explicitly sleep
+     *
+     * @author Dinh Hieu Minh (25minh.dh2@vinuni.edu.vn)
+     * @version 1.0
+     */
+    public static final void COMMUNICATE(boolean isRotten)
+            throws IOException, InterruptedException {
+
         PORT.setBaudRate(BAUD_RATE);
         PORT.setNumDataBits(NUM_DATA_BITS);
         PORT.setNumStopBits(NUM_DATA_STOP_BITS);
         PORT.openPort();
+
         OutputStream outputStream = PORT.getOutputStream();
         byte[] data;
-        if(isRotten){
+
+        if (isRotten) {
             data = "1".getBytes(StandardCharsets.UTF_8);
-        }else{
+        } else {
             data = "0".getBytes(StandardCharsets.UTF_8);
         }
+
         outputStream.write(data);
         outputStream.flush();
     }
-
-//    public static final void RESET_SIGNAL() throws IOException{
-//        doPause(20);
-//        PORT.openPort();
-//        OutputStream outputStream = PORT.getOutputStream();
-//        byte[] data = "0".getBytes(StandardCharsets.UTF_8);
-//        outputStream.write(data);
-//        outputStream.flush();
-//    }
-//
-//    private static void doPause(int ms) {
-//        ScheduledExecutorService scheduledThreadPoolExecutor = Executors.newScheduledThreadPool(10);
-//        try {
-//            scheduledThreadPoolExecutor.schedule(() -> {
-//            }, ms, TimeUnit.MILLISECONDS).get();
-//        } catch (Exception e) {
-//            throw new RuntimeException();
-//        }
-//    }
 }
